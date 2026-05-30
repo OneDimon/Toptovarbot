c0ecda934f8ed5907930fa1d1df9e5ad3c5775f5
OneDimon <147917639+OneDimon@users.noreply.github.com>
2026-05-30 20:07:34 +0300

    feat: register DeliveryOrderDatabase.create_table() in createDb.py

--- tgbot/createDb.py
+++ tgbot/createDb.py
@@ -13,6 +13,7 @@ from database.general.history_transaction import HistoryTransactionDatabase as D
 from database.system.system_info import SystemInfoDatabase as SystemInfoDatabase
 from database.loader.location_loader import LocationLoaderDatabase as DB_location_loader
 from database.loader.confirm_location_seller import ConfirmLocationSellerDatabase as DB_confirm_location_seller
+from database.loader.delivery_order import DeliveryOrderDatabase as DB_delivery_order
 
 async def main():
     await DB_users.create_table_user()
@@ -30,6 +31,7 @@ async def main():
     await SystemInfoDatabase.create_table()
     await DB_location_loader.create_table_location_loader()
     await DB_confirm_location_seller.create_table_confirm_location_seller()
+    await DB_delivery_order.create_table()
     if not await SystemInfoDatabase.get_system_info('system_balance'):
         await SystemInfoDatabase.update_system_balance()
 
